7dbc8f7c1fc63736130e274234aa63e1d24cbb18
Arthur Carlos <arthcrs@gmail.com>
2026-04-21 21:52:27 -0300

    Renamed the template to fit the mod

--- src/main/java/com/mahghuuuls/mahhealthhungertweaks/ExampleMod.java
+++ src/main/java/com/mahghuuuls/mahhealthhungertweaks/ExampleMod.java
@@ -1,6 +1,6 @@
-package com.mahghuuuls.examplemod;
+package com.mahghuuuls.mahhealthhungertweaks;
 
-import com.mahghuuuls.examplemod.proxy.CommonProxy;
+import com.mahghuuuls.mahhealthhungertweaks.proxy.CommonProxy;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
@@ -10,26 +10,26 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = ExampleMod.MOD_ID, name = ExampleMod.NAME, version = ExampleMod.VERSION, dependencies = ExampleMod.DEPENDENCIES)
 public class ExampleMod {
-    public static final String MOD_ID = "examplemod";
-    public static final String NAME = "Example Mod";
+    public static final String MOD_ID = "mahhealthhungertweaks";
+    public static final String NAME = "Mah Health and Hunger Tweaks";
     public static final String VERSION = "1.0.0";
     public static final String DEPENDENCIES = "";
 
-    @SidedProxy(clientSide = "com.mahghuuuls.examplemod.proxy.ClientProxy", serverSide = "com.mahghuuuls.examplemod.proxy.CommonProxy")
+    @SidedProxy(clientSide = "com.mahghuuuls.mahhealthhungertweaks.proxy.ClientProxy", serverSide = "com.mahghuuuls.mahhealthhungertweaks.proxy.CommonProxy")
     public static CommonProxy proxy;
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
-        proxy.preInit(); // On a client, this executes ClientProxy#preInit. On a server, CommonProxy#preInit.
+        proxy.preInit();
     }
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
-        proxy.init(); // On a client, this executes ClientProxy#init. On a server, CommonProxy#init.
+        proxy.init();
     }
 
     @EventHandler
     public void postInit(FMLPostInitializationEvent event) {
-        proxy.postInit(); // On a client, this executes ClientProxy#postInit. On a server, CommonProxy#postInit.
+        proxy.postInit();
     }
 }
--- src/main/java/com/mahghuuuls/mahhealthhungertweaks/proxy/CommonProxy.java
+++ src/main/java/com/mahghuuuls/mahhealthhungertweaks/proxy/CommonProxy.java
@@ -1,19 +1,17 @@
-package com.mahghuuuls.examplemod.proxy;
+package com.mahghuuuls.mahhealthhungertweaks.proxy;
 
-public class ClientProxy extends CommonProxy {
+public class CommonProxy {
 
-	@Override
 	public void preInit() {
 		// Register items, blocks, tile entities, network handlers
 	}
 
-	@Override
 	public void init() {
 		// Register crafting recipes, event handlers
 	}
 
-	@Override
 	public void postInit() {
 		// Inter-mod communication, config finalization
 	}
+
 }
