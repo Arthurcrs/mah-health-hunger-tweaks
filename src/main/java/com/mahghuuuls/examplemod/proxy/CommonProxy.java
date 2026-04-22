8732419fea72a04e4d64e9bc7ea48dd25e023203
Arthur Carlos <arthcrs@gmail.com>
2026-04-21 21:45:01 -0300

    first commit

---
+++ src/main/java/com/mahghuuuls/examplemod/proxy/CommonProxy.java
@@ -0,0 +1,17 @@
+package com.mahghuuuls.examplemod.proxy;
+
+public class CommonProxy {
+
+	public void preInit() {
+		// Register items, blocks, tile entities, network handlers
+	}
+
+	public void init() {
+		// Register crafting recipes, event handlers
+	}
+
+	public void postInit() {
+		// Inter-mod communication, config finalization
+	}
+
+}
